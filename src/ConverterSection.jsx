4d7a3b5844644d2861fa1997aab429f6ba6d1173
anerodata <antonio92.ahr@gmail.com>
2023-07-02 14:12:09 +0200

    Include titles and separate blocks in section

--- src/ConverterSection.jsx
+++ src/ConverterSection.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 const ConverterSection = (props) => {
   return (
     <section>
-      <textarea cols="30" rows="10" onChange={(e) => {
-        const newText = e.target.value
-        props.onTextToReplaceChange(newText)
-      }}>
-      </textarea>
-      <textarea readOnly value={props.valueModified} cols="30" rows="10">
-      </textarea>
+      <div>
+        <h3>Texto</h3>
+        <textarea cols="30" rows="10" onChange={(e) => {
+          const newText = e.target.value
+          props.onTextToReplaceChange(newText)
+        }}>
+        </textarea>
+        <h3>Texto con caracteres Unicode en notación de escape</h3>
+        <textarea readOnly value={props.valueModified} cols="30" rows="10">
+        </textarea>
+      </div>
     </section>
   )
 }
