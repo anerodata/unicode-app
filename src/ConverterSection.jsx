e2698fff3e41a25f2fa841f4b9e831da85e50fcd
anerodata <antonio92.ahr@gmail.com>
2023-07-29 17:05:06 +0200

    Gestion estado selectores con useState

--- src/ConverterSection.jsx
+++ src/ConverterSection.jsx
@@ -1,24 +1,34 @@
 import ConverterSectionFieldset from './ConverterSectionFieldset.jsx'
+import { useState } from 'react'
 const ConverterSection = (props) => {
+  const [ defaultLangFirst, setDefaultLangFirst ] = useState('ES')
+  const [ defaultLangSecond, setDefaultLangSecond ] = useState('EN')
+  const [ defaultLangThird, setDefaultLangThird ] = useState('PT')
   return (
     <section>
       <div>
+  { defaultLangFirst }
+  { defaultLangSecond }
+  { defaultLangThird }
         <ConverterSectionFieldset
           title="Texto"
           onTextToReplaceChange={props.onTextToReplaceChange}
-          defaultLang="ES"
+          defaultLang={defaultLangFirst}
+          onLangChange={(value) => setDefaultLangFirst(value)}
         />
         <ConverterSectionFieldset
           title="Texto con caracteres Unicode en notación de escape"
           value={props.valueModified}
           readOnly={true}
-          defaultLang="EN"
+          defaultLang={defaultLangSecond}
+          onLangChange={(value) => setDefaultLangSecond(value)}
         />
         <ConverterSectionFieldset
           title="Texto con caracteres Unicode en notación de escape"
           value={props.valueModified}
           readOnly={true}
-          defaultLang="PT"
+          defaultLang={defaultLangThird}
+          onLangChange={(value) => setDefaultLangThird(value)}
         />
       </div>
     </section>
--- src/ConverterSectionFieldset.jsx
+++ src/ConverterSectionFieldset.jsx
@@ -3,7 +3,10 @@ const ConverterSection = (props) => {
   return (
     <div>
       <h3>{props.title}</h3>
-      <select defaultValue={props.defaultLang}>
+      <select defaultValue={props.defaultLang} onChange={(e) => {
+        const newValue = e.target.value
+        props.onLangChange(newValue)
+      }}>
         {
           langs.map(lang =>
             <option value={lang.code} key={lang.code}>
