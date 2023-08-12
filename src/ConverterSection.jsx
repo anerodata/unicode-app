457cce2b5d78cfe03c1877374d73eb6199fed594
anerodata <antonio92.ahr@gmail.com>
2023-08-12 13:29:28 +0200

    Refactor: UseEffect for react when input value changes

--- src/ConverterSection.jsx
+++ src/ConverterSection.jsx
@@ -1,56 +1,51 @@
 import ConverterSectionFieldset from './ConverterSectionFieldset.jsx'
-import { useQuery } from "@tanstack/react-query"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import fetchTranslation from './fetchTranslation.jsx'
 const ConverterSection = (props) => {
   const [ defaultLangFirst, setDefaultLangFirst ] = useState('es')
   const [ defaultLangSecond, setDefaultLangSecond ] = useState('en')
   const [ defaultLangThird, setDefaultLangThird ] = useState('pt')
   const [ isLoading, setIsLoading ] = useState(false) 
-  const [ translateParamsToSecondLang, setTranslateParamsToSecondLang ] = useState({
-    query: "",
-    source: defaultLangFirst,
-    target: defaultLangSecond
-  })
-  const debounce = (callback, ms) => {
-    let timerId
-    return (...args) => {
-      clearTimeout(timerId)
-      timerId = setTimeout(() => {
-        alert(timerId)
-        console.log(ms, ...args)
-        callback(...args)
-      }, ms);
-      console.log(timerId)
+  const [ inputValue, setInputValue ] = useState('')
+  useEffect(() => {
+    if (inputValue !== '') {
+      const obj = {
+        query: inputValue,
+        source: defaultLangFirst,
+        target: defaultLangSecond
+      }
+      setIsLoading(true)
+      fetchTranslation(obj).then(res => {
+        console.log(res)
+        props.onTextToReplaceChange(res.translatedText)
+        setIsLoading(false)
+      })
     }
+  }, [inputValue])
+  const handleInputChange = val => {
+    setIsLoading(true)
+    setInputValue(val)
+    setIsLoading(false)
   }
-  // const translationToSecondLang = useQuery([ 'translate', translateParamsToSecondLang ], fetchTranslation)
-  // console.log(translationToSecondLang.data)
   return (
     <section>
       <div>
-  { defaultLangFirst }
-  { defaultLangSecond }
-  { defaultLangThird }
+        { defaultLangFirst }
+        { defaultLangSecond }
+        { defaultLangThird }
         <ConverterSectionFieldset
           title="Texto"
-          onTextToReplaceChange={debounce((value) => {
-            const obj = {
-              query: value,
-              source: defaultLangFirst,
-              target: defaultLangSecond
-            }
-            // setTranslateParamsToSecondLang(obj)
-            props.onTextToReplaceChange(value)
-          }, 2000)}
+          onTextToReplaceChange={handleInputChange}
           defaultLang={defaultLangFirst}
           onLangChange={(value) => setDefaultLangFirst(value)}
+          value={inputValue}
         />
         <ConverterSectionFieldset
           title="Texto con caracteres Unicode en notación de escape"
           value={props.valueModified}
           readOnly={true}
           defaultLang={defaultLangSecond}
+          loading={isLoading}
           onLangChange={(value) => setDefaultLangSecond(value)}
         />
         <ConverterSectionFieldset
@@ -58,6 +53,7 @@ const ConverterSection = (props) => {
           value={props.valueModified}
           readOnly={true}
           defaultLang={defaultLangThird}
+          loading={isLoading}
           onLangChange={(value) => setDefaultLangThird(value)}
         />
       </div>
--- src/fetchTranslation.jsx
+++ src/fetchTranslation.jsx
@@ -1,13 +1,11 @@
-async function fetchTranslation ({ queryKey }) {
+async function fetchTranslation ( queryKey ) {
   console.log(queryKey)
-  const { query, source, target } = queryKey[1]
-  console.log(query, source, target)
   const res = await fetch('https://translate.argosopentech.com/translate', {
     method: 'POST',
     body: JSON.stringify({
-      q: query,
-      source: source,
-      target: target
+      q: queryKey.query,
+      source: queryKey.source,
+      target: queryKey.target
     }),
     headers: { 'Content-Type': 'application/json' }
   });
